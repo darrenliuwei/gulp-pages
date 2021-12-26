95a486aef56c2c096a96f459768fca3074f3677b
darrenliuwei <darrenliuwei@gmail.com>
2021-12-26 16:32:15 +0800

    'gulp-pages'

---
+++ bin/gulp-pages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+process.argv.push("--cwd");
+process.argv.push(process.cwd());
+process.argv.push("--gulpfile");
+process.argv.push(require.resolve(".."));
+
+require("gulp/bin/gulp");
+console.log("gulp-pages");
